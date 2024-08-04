d9f5325ff6f23f5f71e1b83d86cc0f1af18efb99
itsniharsharma <itsniharsharmas@gmail.com>
2024-08-04 18:01:24 +0530

    updated

--- RevisingC.cpp/FunctionsInC.cpp
+++ RevisingC.cpp/FunctionsInC.cpp
@@ -6,7 +6,24 @@ using namespace std;
        cout << country << endl;
    }
 
+   void countEle(int numArray[], int size){
+      for(int i=0; i<size; i++){
+        cout << numArray[i] << endl;
+      }
+   }
+
 int main(){
     myFunction();  //India
     myFunction("USA"); //USA
+
+    //Passing arrays as parameter
+    int arr01[]= {2,3,5,6,7};
+    countEle(arr01, 5);
+
+    return 0;
+
 }
+
+
+
+
