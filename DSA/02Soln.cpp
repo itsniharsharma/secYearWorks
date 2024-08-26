c1fd72243bb3cdc2ffafc689c2de75f577c7eeef
itsniharsharma <itsniharsharmas@gmail.com>
2024-08-26 15:31:49 +0530

    updated

--- DSA/02Soln.cpp
+++ DSA/02Soln.cpp
@@ -1,8 +1,10 @@
 #include<iostream>
+#include<cstring>
+#include<string>
 using namespace std;
 
 //-------------------------- QUES 1 
-
+/*
 int binarySearch(int arr[], int size, int target){
      int start =0;
      int end= size-1;
@@ -49,5 +51,77 @@ int main(){
     
     return 0;
 }
-//-----------------------------------------------------------------
+*/
+//----------------------------------------------------------------- QUES 2 
+//BUBBLE SORT 
+
+/*
+void bubbleSort(int arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+
+
+int main(){
+
+   int arr[] = {64, 34, 25, 12, 22, 11, 90};
+   int size = sizeof(arr)/sizeof(arr[0]);
+
+   bubbleSort(arr, size);
+
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+    
+}
+*/
+
+
+//-----------------------------------------------------------
+
+//LINEAR AND BINARY SEARCH
+
+//-----------------------------------------------------------
+
+/*
+4) String Related Programs 
+
+(a) Write a program to concatenate one string to another string.  
+(b) Write a program to reverse a string.  
+(c) Write a program to delete all the vowels from the string.  
+(d) Write a program to sort the strings in alphabetical order.  
+(e) Write a program to convert a character from uppercase to lowercase. 
+
+*/
+
+
+// A 
+// int main(){
+    
+//     char str1[100]= "hello";
+//     char str2[]= "Nihar";
+
+//     string new1= "I am";
+//     string new2= "student";
+
+//     strcat(str1, str2);
+//     cout << str1 <<endl;
+
+//     new1.append(new2);
+
+//     cout << new1 << endl;
+// }
+
+
+//B 
 
